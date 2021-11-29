36645bb9f68ae777ee87cfa06d80d9268acba05a
Dat <93522149+datvn21@users.noreply.github.com>
2021-11-29 15:42:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,118 +1,32 @@
-console.log("Yuuta")
+<!DOCTYPE html>
+<html lang="en">
 
-function stringToHTML(str) {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(str, 'text/html');
-    return doc.body;
-};
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rikka!</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="https://lh3.googleusercontent.com/KSVD26tPuo5upLLVyvJhS4gYeG9MgoU9F-I4VbuDviWZwITQY0fSJMWf37-VpRgPtezYm8eeloBa0Vn8TKq4gpPa4k7UwBgWxAvgQbb68_qk7cm8ZpP7mzTZ9WKxszHaVnh0nDcVWUSM56PZBXiBapMZIyY8hwC4b9t5ogN2PoinhauwU_3tTnMZ_Ioh15NejqEEkq8VRPK2cYvwy9xxLMYYYcc0AlLH1RPQfQsH6n2Lfs3EuRE9Bu8GNXxsMwtEMP56PfYgMJno3zYBx9mKsfq2QpMubvdIzNkQfPQGlGpf3vZ3jGOidMuKd_9Z8h6cl9PtKjQKbksIoNhjMMqgCkXenwF0Y9jvUYQhpjj2FfkWTHmmf6Dr0CjSgwo_HXmCUn5uf0kSMsuQt8yKJHzWN-Y0Jh4J0z1U-5tDFDkJMalxcw2W_2yHYDRWeBzvvK9H2K1b8uefzvQUEUiwh9_xviwvs0Jw5XxTCvYtY_ER1vMJrOINxBEil1oP59NZBl1gjQ4iwWcwxZ7MKh8jjx2KvhienFJiWh1Jp9dUM6UsWl8mM68UacFDkDW55_f0mMOV5lZKYMxt4r7Ba7sHD5VvqOtTjWUbQM8R6hUwtohmTvy_8kZ2w15wjoG2iNbHWmmg-QexXUzyKst4zGGuSqNRy_NhFSz4tELOiUGulOrXRrtDGWa9QuaRsuVrG-Mi6eXVWAYnsIYC0LQ0vkMbvaIeNUw=w649-h578-no?authuser=0">
+    <script src="script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
 
-function HTMLtoOnlyText(html) {
-    html.innerHTML = html.innerText || html.textContent;
-    return html.innerHTML;
-}
+<body>
+    <button class="mb" onclick="showhide()">Menu</button>
+    <div id="tool" class="tool">
+        <img class="logo" src="https://i.pinimg.com/originals/f5/fe/8a/f5fe8ad01b0b102eea04d07f517909f0.png " alt="Yuuta">
+        <div class="enter">
+            <input placeholder="Enter testId....." type="text" id="name" class="inp">
+            <button class="btn" onclick="run()">Run</button>
+        </div>
 
-function ABCD(num) {
-    if (num == 0) {
-        tag = "A"
-    }
-    if (num == 1) {
-        tag = "B"
-    }
-    if (num == 2) {
-        tag = "C"
-    }
-    if (num == 3) {
-        tag = "D"
-    }
-    if (num == 4) {
-        tag = "E"
-    }
-    if (num == 5) {
-        tag = "F"
-    }
-    return tag;
-}
+    </div>
+    <div id="export" class="export">
+        <div id="load">
+            <img class="loading" src="https://c.tenor.com/uaK34SgLfvMAAAAC/rikka.gif" alt="Yuuta!">
+        </div>
+    </div>
+</body>
 
-function showhide() {
-    document.getElementById('tool').style.display = "flex";
-}
-
-function resetOutput(className) {
-    const elements = document.getElementsByClassName(className);
-    while (elements.length > 0) {
-        elements[0].parentNode.removeChild(elements[0]);
-    }
-}
-
-function run() {
-    resetOutput('questions')
-    console.log("Waitting...");
-    jQuery.getJSON("https://raw.githubusercontent.com/datvn21/lmschecker/main/json%20demo/" + document.getElementById('name').value + ".json").then(data => {
-        console.log(data);
-        console.log("Size of Exam: " + data.questions.length + " questions")
-        var width = $(window).width();
-        if (width <= 600) {
-            document.getElementById('tool').style.display = "none";
-        }
-        document.getElementById('load').style.display = "none";
-        for (let i = 0; i < data.questions.length; i++) {
-            let question = data.questions[i].content
-            question = String(i + 1) + ". " + String(HTMLtoOnlyText(stringToHTML(question))).replace("&nbsp;", "")
-            console.log(question)
-            let qeDiv = document.createElement("div")
-            qeDiv.classList.add('questions')
-            qeDiv.setAttribute('id', 'questions' + String(i))
-            let addQeDiv = document.getElementById('export')
-            let qe = document.createTextNode(question);
-            qeDiv.appendChild(qe)
-            addQeDiv.appendChild(qeDiv)
-            try {
-                for (let z = 0; z < data.questions[i].questions.length; z++) {
-                    let questionChild = data.questions[i].questions[z].content
-                    questionChild = String(z + 1) + ". " + String(HTMLtoOnlyText(stringToHTML(questionChild))).replace("&nbsp;", "")
-                    console.log(questionChild)
-                    let qeChildDiv = document.createElement("div")
-                    qeChildDiv.classList.add('questions-child')
-                    qeChildDiv.setAttribute('id', 'questions-child' + String(z))
-                    let addQeChildDiv = document.getElementById('questions' + String(i))
-                    let qeChild = document.createTextNode(questionChild);
-                    qeChildDiv.appendChild(qeChild)
-                    addQeChildDiv.appendChild(qeChildDiv)
-                    for (let j = 0; j < data.questions[i].questions[z].answers.length; j++) {
-                        let answer = data.questions[i].questions[z].answers[j].content
-                        answer = ABCD(j) + "." + String(HTMLtoOnlyText(stringToHTML(answer))).replace("&nbsp;", "")
-                        console.log(answer)
-                            /*let ansDiv = document.createElement("div")
-                            ansDiv.classList.add('answers')
-                            ansDiv.setAttribute('id', 'answers')
-                            let addAnsDiv = document.getElementById('questions-child' + String(z))
-                            let ans = document.createTextNode(answer);
-                            ansDiv.appendChild(ans)
-                            addAnsDiv.appendChild(ansDiv)*/
-                    }
-                }
-
-            } catch (error) {
-                for (let j = 0; j < data.questions[i].answers.length; j++) {
-                    let answer = data.questions[i].answers[j].content
-                    answer = ABCD(j) + "." + String(HTMLtoOnlyText(stringToHTML(answer))).replace("&nbsp;", "")
-                    console.log(answer)
-                    let ansDiv = document.createElement("div")
-                    if (data.questions[i].answers[j].trueAnswer == 1) {
-                        ansDiv.classList.add('answers-right')
-                    } else {
-                        ansDiv.classList.add('answers')
-                    }
-                    ansDiv.setAttribute('id', 'answers')
-                    let addAnsDiv = document.getElementById('questions' + String(i))
-                    let ans = document.createTextNode(answer);
-                    ansDiv.appendChild(ans)
-                    addAnsDiv.appendChild(ansDiv)
-                }
-            }
-
-        }
-    }).catch(error => {
-        console.error(error);
-    });
-}
+</html>
